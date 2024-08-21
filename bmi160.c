9b3d6d7b18d2f77c0c0f5269d5195b36139777f0
turbq <gron069@gmail.com>
2024-08-21 23:54:33 +0300

    fix chip_id check

--- bmi160.c
+++ bmi160.c
@@ -1490,7 +1490,7 @@ int8_t bmi160_init(struct bmi160_dev *dev)
             rslt = bmi160_get_regs(BMI160_CHIP_ID_ADDR, &dev->chip_id, 1, dev);
         }
 
-        if ((rslt == BMI160_OK) && ((dev->chip_id != BMI160_CHIP_ID)||(dev->chip_id != BMX160_CHIP_ID)))
+        if ((rslt == BMI160_OK) && ((dev->chip_id == BMI160_CHIP_ID)||(dev->chip_id == BMX160_CHIP_ID)))
         {
             dev->any_sig_sel = BMI160_BOTH_ANY_SIG_MOTION_DISABLED;
 
